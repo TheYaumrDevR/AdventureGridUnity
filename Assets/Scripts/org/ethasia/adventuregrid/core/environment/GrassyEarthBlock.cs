4bfc2129c3461d08dd76d9b60bfda40819a1ed4a
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-08-05 23:02:41 +0200

    Added GrassyEarthBlock

---
+++ Assets/Scripts/org/ethasia/adventuregrid/core/environment/GrassyEarthBlock.cs
@@ -0,0 +1,53 @@
+﻿namespace Org.Ethasia.Adventuregrid.Core.Environment
+{
+    public class GrassyEarthBlock : Block
+    {
+        private GrassyEarthBlock() : base(BlockTypes.GRASSY_EARTH)
+        {
+            
+        }
+
+        private static GrassyEarthBlock instance;
+
+        public static GrassyEarthBlock GetInstance()
+        {
+            if (null != instance)
+            {
+                return instance;
+            }
+
+            instance = new GrassyEarthBlock();
+            return instance;
+        }
+
+        public override bool GetFrontFaceIsCovering() 
+        {
+            return true;
+        }
+
+        public override bool GetRightFaceIsCovering()
+        {
+            return true;
+        }
+
+        public override bool GetBackFaceIsCovering()
+        {
+            return true;
+        }
+
+        public override bool GetLeftFaceIsCovering()
+        {
+            return true;
+        }
+
+        public override bool GetBottomFaceIsCovering()
+        {
+            return true;
+        }
+
+        public override bool GetTopFaceIsCovering()
+        {
+            return true;
+        }    
+    }
+}
---
+++ Assets/Tests/org/ethasia/adventuregrid/core/environment/Tests/GrassyEarthBlockTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Org.Ethasia.Adventuregrid.Core.Environment.Tests
+{
+    public class GrassyEarthBlockTest
+    {
+        [UnityTest]
+        public IEnumerator BlockTypeIsAir() 
+        {
+            GrassyEarthBlock testCandidate = GrassyEarthBlock.GetInstance();
+
+            yield return null;
+
+            Assert.That(testCandidate, Is.Not.Null);
+            Assert.That(testCandidate.GetBlockType(), Is.EqualTo(BlockTypes.GRASSY_EARTH));
+        }
+
+        [UnityTest]
+        public IEnumerator BlockFacesAreNotCoveringAnything()
+        {
+            GrassyEarthBlock testCandidate = GrassyEarthBlock.GetInstance();
+
+            yield return null;
+
+            Assert.IsTrue(testCandidate.GetFrontFaceIsCovering());
+            Assert.IsTrue(testCandidate.GetRightFaceIsCovering());
+            Assert.IsTrue(testCandidate.GetBackFaceIsCovering());
+            Assert.IsTrue(testCandidate.GetLeftFaceIsCovering());
+            Assert.IsTrue(testCandidate.GetBottomFaceIsCovering());
+            Assert.IsTrue(testCandidate.GetTopFaceIsCovering());
+        }
+    }
+}
