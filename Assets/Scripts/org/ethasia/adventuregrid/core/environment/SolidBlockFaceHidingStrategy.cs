9ab56dc11ce6f915db7a8ee2ae4f8472532e7399
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-08-06 20:32:07 +0200

    Added SolidBlockFaceHidingStrategy

---
+++ Assets/Scripts/org/ethasia/adventuregrid/core/environment/SolidBlockFaceHidingStrategy.cs
@@ -0,0 +1,26 @@
+﻿namespace Org.Ethasia.Adventuregrid.Core.Environment 
+{
+    public class SolidBlockFaceHidingStrategy : IBlockFaceHidingStrategy
+    {
+        public bool FaceIsHidden(Block coveredBlock, Block coveringBlock, BlockFaceDirections blockFace)
+        {
+            switch (blockFace)
+            {
+                case BlockFaceDirections.FRONT:
+                    return coveringBlock.GetBackFaceIsCovering() && coveredBlock.GetFrontFaceIsCovering();
+                case BlockFaceDirections.RIGHT:
+                    return coveringBlock.GetLeftFaceIsCovering() && coveredBlock.GetRightFaceIsCovering();
+                case BlockFaceDirections.BACK:
+                    return coveringBlock.GetFrontFaceIsCovering() && coveredBlock.GetBackFaceIsCovering();
+                case BlockFaceDirections.LEFT:
+                    return coveringBlock.GetRightFaceIsCovering() && coveredBlock.GetLeftFaceIsCovering();
+                case BlockFaceDirections.BOTTOM:
+                    return coveringBlock.GetTopFaceIsCovering() && coveredBlock.GetBottomFaceIsCovering();
+                case BlockFaceDirections.TOP:
+                    return coveringBlock.GetBottomFaceIsCovering() && coveredBlock.GetTopFaceIsCovering();
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Assets/Tests/org/ethasia/adventuregrid/core/environment/Tests/SolidBlockFaceHidingStrategyTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Org.Ethasia.Adventuregrid.Core.Environment.Tests
+{
+    public class SolidBlockFaceHidingStrategyTest
+    {
+        [UnityTest]
+        public IEnumerator LeftFaceIsHiddenWhenCovered() 
+        {
+            SolidBlockFaceHidingStrategy testCandidate = new SolidBlockFaceHidingStrategy();
+
+            Block coveredBlock = RockBlock.GetInstance();
+            Block coveringBlock = EarthBlock.GetInstance();
+
+            bool result = testCandidate.FaceIsHidden(coveredBlock, coveringBlock, BlockFaceDirections.LEFT);
+
+            yield return null;
+
+            Assert.IsTrue(result);
+        }
+
+        [UnityTest]
+        public IEnumerator LeftFaceIsShownWhenUncovered() 
+        {
+            SolidBlockFaceHidingStrategy testCandidate = new SolidBlockFaceHidingStrategy();
+
+            Block coveredBlock = RockBlock.GetInstance();
+            Block coveringBlock = AirBlock.GetInstance();
+
+            bool result = testCandidate.FaceIsHidden(coveredBlock, coveringBlock, BlockFaceDirections.LEFT);
+
+            yield return null;
+
+            Assert.IsFalse(result);            
+        }
+
+        [UnityTest]
+        public IEnumerator LeftFaceIsShownWhenItsNotCovering() 
+        {
+            SolidBlockFaceHidingStrategy testCandidate = new SolidBlockFaceHidingStrategy();
+
+            Block coveredBlock = AirBlock.GetInstance();
+            Block coveringBlock = EarthBlock.GetInstance();
+
+            bool result = testCandidate.FaceIsHidden(coveredBlock, coveringBlock, BlockFaceDirections.LEFT);
+
+            yield return null;
+
+            Assert.IsFalse(result);
+        }        
+    }
+}
