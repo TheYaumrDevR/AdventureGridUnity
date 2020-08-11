95a8ffb38c1eda6e89c1847f49713aa2dd99ad8d
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-08-11 23:56:08 +0200

    Added fast math floor function implementation.

---
+++ Assets/Scripts/org/ethasia/adventuregrid/core/math/FastMath.cs
@@ -0,0 +1,11 @@
+﻿namespace Org.Ethasia.Adventuregrid.Core.Math
+{
+    public class FastMath
+    {
+        public static int Floor(double x) 
+        {
+            int xCast = (int)x;
+            return x < xCast ? xCast - 1 : xCast;            
+        }
+    }
+}
