7c337a13ef9b42faa1cc0f8a07ccd9b6e336a6bc
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-08-11 23:49:22 +0200

    Added class for 3D vectors.

---
+++ Assets/Scripts/org/ethasia/adventuregrid/core/math/Vector3.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace Org.Ethasia.Adventuregrid.Core.Math
+{
+    public class Vector3
+    {
+        public static readonly Vector3 UNIT_X = new Vector3(1.0f, 0.0f, 0.0f);
+        public static readonly Vector3 UNIT_Y = new Vector3(0.0f, 1.0f, 0.0f);
+        public static readonly Vector3 UNIT_Z = new Vector3(0.0f, 0.0f, 1.0f);
+
+        private float x, y, z;
+
+        public float GetX() 
+        {
+            return x;
+        }
+    
+        public float GetY() 
+        {
+            return y;
+        } 
+    
+        public float GetZ() 
+        {
+            return z;
+        }     
+
+        public Vector3 Set(float x, float y, float z) 
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        
+            return this;
+        }     
+
+        private float bufferedResultX, bufferedResultY, bufferedResultZ;     
+
+        public float GetBufferedResultX() 
+        {
+            return bufferedResultX;
+        }
+
+        public float GetBufferedResultY() 
+        {
+            return bufferedResultY;
+        }   
+    
+        public float GetBufferedResultZ() 
+        {
+            return bufferedResultZ;
+        }         
+
+        public Vector3(float x, float y, float z) 
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public float GetSquaredLength() 
+        {
+            return x * x + y * y + z * z;
+        } 
+
+        public Vector3 Scale(float scalar) 
+        {
+            x *= scalar;
+            y *= scalar;
+            z *= scalar;
+        
+            return this;
+        }
+
+        public Vector3 ScaleImmutable(float scalar) 
+        {
+            float scaledX = x * scalar;
+            float scaledY  = y * scalar;
+            float scaledZ  = z * scalar;
+        
+            return new Vector3(scaledX, scaledY, scaledZ);
+        }   
+
+        public Vector3 Normalize() 
+        {
+            float squaredLength = GetSquaredLength();
+            if (0.0f == squaredLength || 1.0f == squaredLength) 
+            {
+                return this;
+            }
+        
+            return Scale(1.0f / (float)System.Math.Sqrt(squaredLength));
+        }
+
+        public Vector3 Add(Vector3 other) 
+        {
+            x += other.x;
+            y += other.y;
+            z += other.z;
+        
+            return this;
+        } 
+
+        public Vector3 AddImmutable(Vector3 other) 
+        {
+            float resultX = x + other.x;
+            float resultY = y + other.y;
+            float resultZ = z + other.z;
+        
+            return new Vector3(resultX, resultY, resultZ);
+        }
+
+        public void AddImmutableBufferResult(Vector3 other) 
+        {
+            bufferedResultX = x + other.x;
+            bufferedResultY = y + other.y;
+            bufferedResultZ = z + other.z;
+        }             
+
+        public Vector3 Subtract(Vector3 other) 
+        {
+            x -= other.x;
+            y -= other.y;
+            z -= other.z;
+        
+            return this;
+        } 
+
+        public Vector3 SubtractImmutable(Vector3 other) 
+        {
+            float resultX = x - other.x;
+            float resultY = y - other.y;
+            float resultZ = z - other.z;
+        
+            return new Vector3(resultX, resultY, resultZ);
+        }     
+
+        public float Dot(Vector3 other) 
+        {
+            return x * other.x + y * other.y + z * other.z;
+        }    
+
+        public Vector3 Cross(Vector3 other) 
+        {
+            float newX = y * other.z - z * other.y;
+            float newY = z * other.x - x * other.z;
+            float newZ = x * other.y - y * other.x;
+        
+            x = newX;
+            y = newY;
+            z = newZ;
+        
+            return this;
+        }                                                              
+    }
+}
---
+++ Assets/Tests/org/ethasia/adventuregrid/core/math/MathTests/Vector3Test.cs
@@ -0,0 +1,221 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Org.Ethasia.Adventuregrid.Core.Math.MathTests
+{
+    public class Vector3Test
+    {
+        [UnityTest]
+        public IEnumerator GettersRetrieveSetComponents() 
+        {
+            Vector3 testCandidate = new Vector3(1.0f, 2.0f, 3.0f);
+
+            yield return null;
+        
+            Assert.That(testCandidate.GetX(), Is.EqualTo(1.0f));
+            Assert.That(testCandidate.GetY(), Is.EqualTo(2.0f));
+            Assert.That(testCandidate.GetZ(), Is.EqualTo(3.0f));
+        }
+
+        [UnityTest]
+        public IEnumerator SetsComponentsProperly() 
+        {
+            Vector3 testCandidate = new Vector3(1.0f, 2.0f, 3.0f);
+            Vector3 changedVector = testCandidate.Set(4.5f, 3.2f, 1.5f);
+        
+            yield return null;
+
+            Assert.That(changedVector.GetX(), Is.EqualTo(4.5f));
+            Assert.That(changedVector.GetY(), Is.EqualTo(3.2f));
+            Assert.That(changedVector.GetZ(), Is.EqualTo(1.5f));
+        } 
+
+        [UnityTest]
+        public IEnumerator SquaredLengthIsCalculatedProperly() 
+        {
+            Vector3 testCandidate = new Vector3(4.5f, 3.2f, 1.5f);   
+        
+            float expected = 4.5f * 4.5f + 3.2f * 3.2f + 1.5f * 1.5f;
+
+            yield return null;
+
+            Assert.That(testCandidate.GetSquaredLength(), Is.EqualTo(expected));
+        }  
+
+        [UnityTest]
+        public IEnumerator ScalesCorrectly() 
+        {
+            Vector3 testCandidate = new Vector3(2.2f, 5.1f, 3.7f);
+        
+            float expectedX = 2.2f * 0.6f;
+            float expectedY = 5.1f * 0.6f;
+            float expectedZ = 3.7f * 0.6f;
+        
+            testCandidate.Scale(0.6f);
+
+            yield return null;
+
+            Assert.That(testCandidate.GetX(), Is.EqualTo(expectedX));
+            Assert.That(testCandidate.GetY(), Is.EqualTo(expectedY));
+            Assert.That(testCandidate.GetZ(), Is.EqualTo(expectedZ));                
+        } 
+
+        [UnityTest]
+        public IEnumerator ImmutableScaleDoesNotChangeVector() 
+        {
+            Vector3 testCandidate = new Vector3(4.9f, 2.3f, 0.3f);
+        
+            float expectedX = 4.9f * 0.4f;
+            float expectedY = 2.3f * 0.4f;
+            float expectedZ = 0.3f * 0.4f;   
+        
+            Vector3 result = testCandidate.ScaleImmutable(0.4f);
+
+            yield return null;
+
+            Assert.That(testCandidate.GetX(), Is.EqualTo(4.9f));
+            Assert.That(testCandidate.GetY(), Is.EqualTo(2.3f));
+            Assert.That(testCandidate.GetZ(), Is.EqualTo(0.3f));   
+        
+            Assert.That(result.GetX(), Is.EqualTo(expectedX));
+            Assert.That(result.GetY(), Is.EqualTo(expectedY));
+            Assert.That(result.GetZ(), Is.EqualTo(expectedZ));             
+        }
+
+        [UnityTest]
+        public IEnumerator NormalizeSetsVectorLengthToOne() 
+        {    
+            Vector3 testCandidate = new Vector3(2.2f, 5.1f, 3.7f);
+        
+            testCandidate.Normalize();
+            float result = testCandidate.GetSquaredLength();
+
+            yield return null;
+
+            Assert.That(result, Is.EqualTo(1.0f));
+        }  
+
+        [UnityTest]
+        public IEnumerator AddingAddsVectorComponents() 
+        {  
+            Vector3 testCandidate = new Vector3(2.2f, 5.1f, 3.7f);
+            Vector3 toAdd = new Vector3(1.0f, 2.0f, 3.0f);
+        
+            testCandidate = testCandidate.Add(toAdd);
+
+            yield return null;
+
+            Assert.That(testCandidate.GetX(), Is.EqualTo(3.2f));
+            Assert.That(testCandidate.GetY(), Is.EqualTo(7.1f));
+            Assert.That(testCandidate.GetZ(), Is.EqualTo(6.7f));               
+        }   
+
+        [UnityTest]
+        public IEnumerator SubtractingVectorsSubtractsComponents() 
+        {  
+            Vector3 testCandidate = new Vector3(2.2f, 5.1f, 3.7f);
+            Vector3 toSubtract = new Vector3(1.0f, 2.0f, 3.0f);
+
+            testCandidate = testCandidate.Subtract(toSubtract);  
+
+            yield return null;
+
+            Assert.That(System.Math.Abs(1.2f - testCandidate.GetX()), Is.LessThan(0.001f));
+            Assert.That(System.Math.Abs(3.1f - testCandidate.GetY()), Is.LessThan(0.001f));
+            Assert.That(System.Math.Abs(0.7f - testCandidate.GetZ()), Is.LessThan(0.001f));
+        }   
+
+        [UnityTest]
+        public IEnumerator DotProductIsCalculatedCorrectly() 
+        {  
+            Vector3 testCandidate = new Vector3(2.2f, 5.1f, 3.7f);
+            Vector3 secondVector = new Vector3(4.5f, 3.2f, 1.5f); 
+        
+            float result = testCandidate.Dot(secondVector);
+            float expected = 2.2f * 4.5f + 5.1f * 3.2f + 3.7f * 1.5f;
+
+            yield return null;
+
+            Assert.That(result, Is.EqualTo(expected));
+        } 
+
+        [UnityTest]
+        public IEnumerator CrossProductIsCalculatedCorrectly() 
+        {  
+            Vector3 testCandidate = new Vector3(2.2f, 5.1f, 3.7f);
+            Vector3 secondVector = new Vector3(4.5f, 3.2f, 1.5f); 
+
+            testCandidate = testCandidate.Cross(secondVector);
+        
+            float expectedX = 5.1f * 1.5f - 3.7f * 3.2f;
+            float expectedY = 3.7f * 4.5f - 2.2f * 1.5f;
+            float expectedZ = 2.2f * 3.2f - 5.1f * 4.5f; 
+
+            yield return null;
+
+            Assert.That(System.Math.Abs(expectedX - testCandidate.GetX()), Is.LessThan(0.001f));
+            Assert.That(System.Math.Abs(expectedY - testCandidate.GetY()), Is.LessThan(0.001f));
+            Assert.That(System.Math.Abs(expectedZ - testCandidate.GetZ()), Is.LessThan(0.001f));          
+        }  
+
+        [UnityTest]
+        public IEnumerator ImmutableAdditionDoesNotChangeOriginalVector() 
+        { 
+            Vector3 testCandidate = new Vector3(1.5f, 9.1f, 4.3f);
+            Vector3 toAdd = new Vector3(2.0f, 4.0f, 1.0f);
+        
+            Vector3 result = testCandidate.AddImmutable(toAdd);
+
+            yield return null;
+
+            Assert.That(testCandidate.GetX(), Is.EqualTo(1.5f));
+            Assert.That(testCandidate.GetY(), Is.EqualTo(9.1f));
+            Assert.That(testCandidate.GetZ(), Is.EqualTo(4.3f));
+        
+            Assert.That(result.GetX(), Is.EqualTo(3.5f));
+            Assert.That(result.GetY(), Is.EqualTo(13.1f));
+            Assert.That(result.GetZ(), Is.EqualTo(5.3f));            
+        }  
+
+        [UnityTest]
+        public IEnumerator ImmutableSubtractionDoesNotChangeOriginalVector() 
+        { 
+            Vector3 testCandidate = new Vector3(1.5f, 9.1f, 4.3f);
+            Vector3 toSubtract = new Vector3(2.0f, 4.0f, 1.0f);
+
+            Vector3 result = testCandidate.SubtractImmutable(toSubtract);
+
+            yield return null;
+
+            Assert.That(testCandidate.GetX(), Is.EqualTo(1.5f));
+            Assert.That(testCandidate.GetY(), Is.EqualTo(9.1f));
+            Assert.That(testCandidate.GetZ(), Is.EqualTo(4.3f));     
+        
+            Assert.That(System.Math.Abs(-0.5f - result.GetX()), Is.LessThan(0.001f));
+            Assert.That(System.Math.Abs(5.1f - result.GetY()), Is.LessThan(0.001f));
+            Assert.That(System.Math.Abs(3.3f - result.GetZ()), Is.LessThan(0.001f));           
+        }  
+
+        [UnityTest]
+        public IEnumerator ResultBufferIsFilledAfterBufferedAddition() 
+        { 
+            Vector3 testCandidate = new Vector3(1.5f, 9.1f, 4.3f);
+            Vector3 toAdd = new Vector3(2.0f, 4.0f, 1.0f);
+        
+            testCandidate.AddImmutableBufferResult(toAdd);
+
+            yield return null;
+
+            Assert.That(testCandidate.GetBufferedResultX(), Is.EqualTo(3.5f));
+            Assert.That(testCandidate.GetBufferedResultY(), Is.EqualTo(13.1f));
+            Assert.That(testCandidate.GetBufferedResultZ(), Is.EqualTo(5.3f));    
+        
+            Assert.That(testCandidate.GetX(), Is.EqualTo(1.5f));
+            Assert.That(testCandidate.GetY(), Is.EqualTo(9.1f));
+            Assert.That(testCandidate.GetZ(), Is.EqualTo(4.3f));              
+        }                                                                         
+    }
+}
