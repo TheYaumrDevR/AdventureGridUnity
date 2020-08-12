7bbe1fee200c69d8656afe572392eca6f1f18f66
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-08-12 22:06:16 +0200

    Fixed formatting.

--- Assets/Scripts/org/ethasia/adventuregrid/core/math/PerlinNoise2D.cs
+++ Assets/Scripts/org/ethasia/adventuregrid/core/math/PerlinNoise2D.cs
@@ -71,11 +71,11 @@ namespace Org.Ethasia.Adventuregrid.Core.Math
 
         private static double Grad(int hash, double x, double y, double z) 
         {
-        int h = hash & 15;
-        double u = h < 8 ? x : y,                 
-            v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+            int h = hash & 15;
+            double u = h < 8 ? x : y,                 
+                v = h < 4 ? y : h == 12 || h == 14 ? x : z;
       
-        return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);        
+            return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);        
         }    
     }
 }
