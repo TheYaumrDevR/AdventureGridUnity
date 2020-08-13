89ab0ef880ac12f340d431c4cd0fe836b63e6781
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-08-13 22:13:23 +0200

    Added a generator for the main menu scene.

---
+++ Assets/Scripts/org/ethasia/adventuregrid/core/environment/MainMenuSceneIslandGenerator.cs
@@ -0,0 +1,40 @@
+﻿namespace Org.Ethasia.Adventuregrid.Core.Environment
+{
+
+    public class MainMenuSceneIslandGenerator
+    {
+
+        public Island CreateIslandForMainMenuScene()
+        {
+            Island result = new Island(20);
+
+            Block rockBlock = RockBlock.GetInstance();
+            Block earthBlock = EarthBlock.GetInstance();
+            Block grassBlock = GrassyEarthBlock.GetInstance();
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if (k == 0)
+                        {
+                            result.PlaceBlockAt(rockBlock, i, k, j);
+                        }
+                        else if (k == 1)
+                        {
+                            result.PlaceBlockAt(earthBlock, i, k, j);
+                        }
+                        else 
+                        {
+                            result.PlaceBlockAt(grassBlock, i, k, j);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
