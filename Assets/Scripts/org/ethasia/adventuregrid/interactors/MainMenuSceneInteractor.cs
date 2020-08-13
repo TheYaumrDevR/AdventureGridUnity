0177a43d69ada86e257a8dde0a986ecd08fd4a41
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-08-13 22:30:37 +0200

    Added main menu scene interactor.

---
+++ Assets/Scripts/org/ethasia/adventuregrid/interactors/MainMenuSceneInteractor.cs
@@ -0,0 +1,12 @@
+﻿namespace Org.Ethasia.Adventuregrid.Interactors
+{
+    public class MainMenuSceneInteractor
+    {
+        public void GenerateMainMenuSceneAndEnterMainMenu()
+        {
+            MainMenuSceneIslandGenerator sceneMapGenerator = new MainMenuSceneIslandGenerator();
+            Island sceneMap = sceneMapGenerator.CreateIslandForMainMenuScene();
+            // TODO: Render scene map
+        }
+    }
+}
