2eec6d67c16626b34da70dc24ed8e158b01b57e5
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-10-14 20:46:49 +0200

    Added PortalBlock

---
+++ Assets/Scripts/org/ethasia/adventuregrid/core/environment/PortalBlock.cs
@@ -0,0 +1,54 @@
+﻿namespace Org.Ethasia.Adventuregrid.Core.Environment
+{
+    public class PortalBlock : Block
+    {
+        private PortalBlock() : base(BlockTypes.PORTAL)
+        {
+            faceHidingStrategy = new TransparentBlockFaceHidingStrategy();
+        }
+
+        private static PortalBlock instance;
+    
+        public static PortalBlock GetInstance() 
+        {
+
+            if (null != instance) 
+            {
+                return instance;
+            }        
+        
+            instance = new PortalBlock();
+            return instance;
+        }
+    
+        public override bool GetRightFaceIsCovering() 
+        {
+            return false;
+        }
+
+        public override bool GetFrontFaceIsCovering() 
+        {
+            return false;
+        }
+
+        public override bool GetLeftFaceIsCovering() 
+        {
+            return false;
+        }
+
+        public override bool GetBackFaceIsCovering() 
+        {
+            return false;
+        }
+
+        public override bool GetBottomFaceIsCovering() 
+        {
+            return false;
+        }
+
+        public override bool GetTopFaceIsCovering() 
+        {
+            return false;
+        }          
+    }
+}
---
+++ Assets/Scripts/org/ethasia/adventuregrid/core/environment/TransparentBlockFaceHidingStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace Org.Ethasia.Adventuregrid.Core.Environment 
+{
+    public class TransparentBlockFaceHidingStrategy : IBlockFaceHidingStrategy
+    {
+        public bool FaceIsHidden(Block coveredBlock, Block coveringBlock, BlockFaceDirections blockFace)
+        {
+            return coveringBlock.GetBlockType() != BlockTypes.AIR;
+        }
+    }
+}
