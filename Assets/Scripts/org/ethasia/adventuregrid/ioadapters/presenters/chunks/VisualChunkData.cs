39f5962a1f0376f1fbde754ef7c70b87b03b1850
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-10-14 20:26:37 +0200

    Added VisualChunkData with tets

---
+++ Assets/Scripts/org/ethasia/adventuregrid/ioadapters/presenters/chunks/VisualChunkData.cs
@@ -0,0 +1,186 @@
+﻿namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks
+{
+    public class VisualChunkData
+    {
+        #region Fields
+
+        private int worldX;
+        private int worldY;
+
+        private float[][] verticesOfBlocks;
+        private int[][] indicesOfBlocks;
+        private float[][] normalsOfBlocks;
+        private float[][] uvCoordinatesOfBlocks;
+
+        private int currentVertexAmount;
+        private int currentIndexAmount;
+        private int currentNormalsAmount;
+        private int currentUvAmount;
+
+        private int numberOfTimesVerticesAdded;
+        private int numberOfTimesIndicesAdded;
+        private int numberOfTimesNormalsAdded;
+        private int numberOfTimesUvAdded;
+
+        private float[] allVerticesFlattened;
+        private int[] allIndicesFlattened;
+        private float[] allNormalsFlattened;
+        private float[] allUvCoordinatesFlattened;
+
+        #endregion
+
+        #region Methods
+
+        public bool IsOpaqueChunk
+        {
+            get;
+            set;
+        }
+
+        public void SetWorldPosition(int x, int y)
+        {   
+            worldX = x;
+            worldY = y;
+        }
+
+        public int GetWorldX() 
+        {
+            return worldX;
+        }
+
+        public int GetWorldY()
+        {
+            return worldY;
+        }
+
+        public float[] GetVertices() 
+        {
+            return allVerticesFlattened;
+        }
+
+        public int[] GetIndices() 
+        {
+            return allIndicesFlattened;
+        }
+
+        public float[] GetNormals()
+        {
+            return allNormalsFlattened;
+        }
+
+        public float[] GetUvCoordinates()
+        {
+            return allUvCoordinatesFlattened;
+        }
+
+        public void SetUpWithNumberOfBlocksInChunk(int numberOfBlocksInChunk) 
+        {
+            verticesOfBlocks = new float[numberOfBlocksInChunk][];
+            indicesOfBlocks = new int[numberOfBlocksInChunk][];
+            normalsOfBlocks = new float[numberOfBlocksInChunk][];
+            uvCoordinatesOfBlocks = new float[numberOfBlocksInChunk][];
+        }
+
+        public void AddVerticesToTemporaryBuffer(float[] vertices) 
+        {
+            verticesOfBlocks[numberOfTimesVerticesAdded] = vertices;
+            currentVertexAmount += vertices.Length;
+            numberOfTimesVerticesAdded++;        
+        }   
+
+        public void AddIndicesToTemporaryBuffer(int[] indices) 
+        {
+            indicesOfBlocks[numberOfTimesIndicesAdded] = indices;
+            currentIndexAmount += indices.Length;   
+            numberOfTimesIndicesAdded++;
+        } 
+
+        public void AddNormalsToTemporaryBuffer(float[] normals) 
+        {
+            normalsOfBlocks[numberOfTimesNormalsAdded] = normals;
+            currentNormalsAmount += normals.Length;
+            numberOfTimesNormalsAdded++;        
+        }  
+
+        public void AddUvCoordinatesToTemporaryBuffer(float[] uvCoordinates) 
+        {
+            uvCoordinatesOfBlocks[numberOfTimesUvAdded] = uvCoordinates;
+            currentUvAmount += uvCoordinates.Length;
+            numberOfTimesUvAdded++;        
+        }   
+
+        public void BuildChunkData() 
+        {
+            FlattenVertices();
+            FlattenIndices();
+            FlattenNormals();
+            FlattenUvs();
+        }                                
+
+        #endregion
+
+        #region Helper Methods
+
+        private void FlattenVertices() 
+        {
+            allVerticesFlattened = new float[currentVertexAmount];
+            int k = 0;  
+        
+            for (int i = 0; i < verticesOfBlocks.Length; i++) 
+            {
+                for (int j = 0; j < verticesOfBlocks[i].Length; j++) 
+                {
+                    allVerticesFlattened[k] = verticesOfBlocks[i][j];
+                    k++;
+                }
+            }        
+        }      
+
+        private void FlattenIndices() 
+        {
+            allIndicesFlattened = new int[currentIndexAmount];
+            int k = 0;  
+        
+            for (int i = 0; i < indicesOfBlocks.Length; i++) 
+            {
+                for (int j = 0; j < indicesOfBlocks[i].Length; j++) 
+                {
+                    allIndicesFlattened[k] = indicesOfBlocks[i][j];
+                    k++;
+                }
+            }        
+        }  
+
+        private void FlattenNormals() 
+        {
+            allNormalsFlattened = new float[currentNormalsAmount];
+            int k = 0;  
+        
+            for (int i = 0; i < normalsOfBlocks.Length; i++) 
+            {
+                for (int j = 0; j < normalsOfBlocks[i].Length; j++) 
+                {
+                    allNormalsFlattened[k] = normalsOfBlocks[i][j];
+                    k++;
+                }
+            }           
+        }
+
+        private void FlattenUvs() 
+        {
+            allUvCoordinatesFlattened = new float[currentUvAmount];
+            int k = 0;  
+        
+            for (int i = 0; i < uvCoordinatesOfBlocks.Length; i++) 
+            {
+                for (int j = 0; j < uvCoordinatesOfBlocks[i].Length; j++) 
+                {
+                    allUvCoordinatesFlattened[k] = uvCoordinatesOfBlocks[i][j];
+                    k++;
+                }
+            }        
+        }                          
+
+        #endregion
+    }
+}
---
+++ Assets/Tests/org/ethasia/adventuregrid/ioadapters/presenters/chunks/ChunkPresentingTests/VisualChunkDataTest.cs
@@ -0,0 +1,173 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks.ChunkPresentingTests
+{
+    public class VisualChunkDataTests
+    {
+        [UnityTest]
+        public IEnumerator WorldPositionCanBeSet() 
+        {
+            VisualChunkData testCandidate = new VisualChunkData();
+
+            testCandidate.SetWorldPosition(5, 9);
+
+            yield return null;
+
+            Assert.That(testCandidate.GetWorldX(), Is.EqualTo(5));
+            Assert.That(testCandidate.GetWorldY(), Is.EqualTo(9));         
+        }
+
+        [UnityTest]
+        public IEnumerator FlattenedVerticesAreCalculatedFromTemporaryBuffer() 
+        {
+            VisualChunkData testCandidate = new VisualChunkData();
+        
+            float[] vertices = { 0.2f, 0.3f, 0.4f, 0.3f, 0.7f, 0.1f };
+            float[] secondVertices = { 0.3f, 0.4f, 0.5f, 0.6f, 0.8f, 0.3f };
+        
+            testCandidate.SetUpWithNumberOfBlocksInChunk(2);
+            testCandidate.AddVerticesToTemporaryBuffer(vertices);
+            testCandidate.AddVerticesToTemporaryBuffer(secondVertices);
+            testCandidate.AddIndicesToTemporaryBuffer(new int[0]);
+            testCandidate.AddIndicesToTemporaryBuffer(new int[0]);
+            testCandidate.AddNormalsToTemporaryBuffer(new float[0]);
+            testCandidate.AddNormalsToTemporaryBuffer(new float[0]);  
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(new float[0]);
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(new float[0]);      
+        
+            testCandidate.BuildChunkData();
+            float[] result = testCandidate.GetVertices();
+        
+            yield return null;
+
+            Assert.That(result.Length, Is.EqualTo(12));
+            Assert.That(result[0], Is.EqualTo(0.2f));
+            Assert.That(result[1], Is.EqualTo(0.3f));
+            Assert.That(result[2], Is.EqualTo(0.4f));
+            Assert.That(result[3], Is.EqualTo(0.3f));
+            Assert.That(result[4], Is.EqualTo(0.7f));
+            Assert.That(result[5], Is.EqualTo(0.1f));
+            Assert.That(result[6], Is.EqualTo(0.3f));
+            Assert.That(result[7], Is.EqualTo(0.4f));    
+            Assert.That(result[8], Is.EqualTo(0.5f));
+            Assert.That(result[9], Is.EqualTo(0.6f));
+            Assert.That(result[10], Is.EqualTo(0.8f));
+            Assert.That(result[11], Is.EqualTo(0.3f));
+        } 
+
+        [UnityTest]
+        public IEnumerator FlattenedIndicesAreCalculatedFromTemporaryBuffer() 
+        {
+            VisualChunkData testCandidate = new VisualChunkData();
+        
+            int[] indices = { 2, 4 };
+            int[] secondIndices = { 5, 7 };
+            int[] thirdIndices = { 9, 8 };
+        
+            testCandidate.SetUpWithNumberOfBlocksInChunk(3);
+            testCandidate.AddIndicesToTemporaryBuffer(indices);
+            testCandidate.AddIndicesToTemporaryBuffer(secondIndices);
+            testCandidate.AddIndicesToTemporaryBuffer(thirdIndices);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);
+            testCandidate.AddNormalsToTemporaryBuffer(new float[0]);
+            testCandidate.AddNormalsToTemporaryBuffer(new float[0]);
+            testCandidate.AddNormalsToTemporaryBuffer(new float[0]); 
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(new float[0]);
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(new float[0]);
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(new float[0]);        
+        
+            testCandidate.BuildChunkData();
+            int[] result = testCandidate.GetIndices();
+
+            yield return null;
+
+            Assert.That(result.Length, Is.EqualTo(6));
+            Assert.That(result[0], Is.EqualTo(2));
+            Assert.That(result[1], Is.EqualTo(4));
+            Assert.That(result[2], Is.EqualTo(5));
+            Assert.That(result[3], Is.EqualTo(7));
+            Assert.That(result[4], Is.EqualTo(9));
+            Assert.That(result[5], Is.EqualTo(8));              
+        }
+
+        [UnityTest]
+        public IEnumerator FlattenedNormalsAreCalculatedFromTemporaryBuffer() 
+        {
+            VisualChunkData testCandidate = new VisualChunkData();
+        
+            float[] normals = { 1.4f, 2.3f };
+            float[] secondNormals = { 5.4f, 7.1f };
+            float[] thirdNormals = { 0.4f, 1.5f };     
+        
+            testCandidate.SetUpWithNumberOfBlocksInChunk(3);
+            testCandidate.AddNormalsToTemporaryBuffer(normals);
+            testCandidate.AddNormalsToTemporaryBuffer(secondNormals);
+            testCandidate.AddNormalsToTemporaryBuffer(thirdNormals);
+            testCandidate.AddIndicesToTemporaryBuffer(new int[0]);
+            testCandidate.AddIndicesToTemporaryBuffer(new int[0]);
+            testCandidate.AddIndicesToTemporaryBuffer(new int[0]);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);   
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(new float[0]);
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(new float[0]);
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(new float[0]);
+        
+            testCandidate.BuildChunkData();
+            float[] result = testCandidate.GetNormals();    
+
+            yield return null;     
+
+            Assert.That(result.Length, Is.EqualTo(6));
+            Assert.That(result[0], Is.EqualTo(1.4f));
+            Assert.That(result[1], Is.EqualTo(2.3f));
+            Assert.That(result[2], Is.EqualTo(5.4f));
+            Assert.That(result[3], Is.EqualTo(7.1f));
+            Assert.That(result[4], Is.EqualTo(0.4f));
+            Assert.That(result[5], Is.EqualTo(1.5f));           
+        }
+
+        [UnityTest]
+        public IEnumerator FlattenedUvCoordinatesAreCalculatedFromTemporaryBuffer() 
+        {
+            VisualChunkData testCandidate = new VisualChunkData();
+        
+            float[] uvCoordinates = { 0.4f, 1.5f };
+            float[] secondUvCoordinates = { 9.1f, 4.4f };
+            float[] thirdUvCoordinates = { 7.7f, 8.3f };     
+        
+            testCandidate.SetUpWithNumberOfBlocksInChunk(3);
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(uvCoordinates);
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(secondUvCoordinates);
+            testCandidate.AddUvCoordinatesToTemporaryBuffer(thirdUvCoordinates);
+            testCandidate.AddNormalsToTemporaryBuffer(new float[0]);
+            testCandidate.AddNormalsToTemporaryBuffer(new float[0]);
+            testCandidate.AddNormalsToTemporaryBuffer(new float[0]);
+            testCandidate.AddIndicesToTemporaryBuffer(new int[0]);
+            testCandidate.AddIndicesToTemporaryBuffer(new int[0]);
+            testCandidate.AddIndicesToTemporaryBuffer(new int[0]);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);
+            testCandidate.AddVerticesToTemporaryBuffer(new float[0]);   
+        
+            testCandidate.BuildChunkData();
+            float[] result = testCandidate.GetUvCoordinates();
+
+            yield return null; 
+
+            Assert.That(result.Length, Is.EqualTo(6));
+            Assert.That(result[0], Is.EqualTo(0.4f));
+            Assert.That(result[1], Is.EqualTo(1.5f));
+            Assert.That(result[2], Is.EqualTo(9.1f));
+            Assert.That(result[3], Is.EqualTo(4.4f));
+            Assert.That(result[4], Is.EqualTo(7.7f));
+            Assert.That(result[5], Is.EqualTo(8.3f));                     
+        }                               
+    }    
+}
