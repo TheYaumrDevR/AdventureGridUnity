c8c1ae28d16e4ecd0ab8bbd03e239e41ee369c9a
RASTISLAV ONDREJKA <rastislav.ondrejka@capgemini.com>
2021-03-01 19:02:21 +0100

    add: StandardIslandPresenter with chunks length definition

---
+++ Assets/Scripts/org/ethasia/adventuregrid/ioadapters/presenters/StandardIslandPresenter.cs
@@ -0,0 +1,7 @@
+﻿namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters 
+{
+    public class StandardIslandPresenter
+    {
+        public static readonly int CHUNK_EDGE_LENGTH_IN_BLOCKS = 16;
+    }
+}
---
+++ Assets/Tests/org/ethasia/adventuregrid/ioadapters/presenters/PresentersTests/StandardIslandPresenterTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.PresentersTests
+{
+    public class StandardIslandPresenterTest
+    {
+        [Test]
+        public void TestThatIslandIsPresentedInChunks()
+        {
+            StandardIslandPresenter testCandidate = new StandardIslandPresenter();
+        }
+    }
+}
