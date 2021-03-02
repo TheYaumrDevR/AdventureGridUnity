f68a5c072ab9f41a0599d1d975d6aa5a4c5376a6
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2021-03-02 22:12:26 +0100

    Added StandardBlockVisualsBuilder with empty Build method and Getters

---
+++ Assets/Scripts/org/ethasia/adventuregrid/ioadapters/presenters/chunks/StandardBlockVisualsBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks
+{
+    public class StandardBlockVisualsBuilder
+    {
+        public void Build() 
+        {
+
+        }
+
+        public float[] GetShapePositions() 
+        {
+            return new float[0];
+        }
+
+        public int[] GetShapeIndices() 
+        {
+            return new int[0];
+        }
+
+        public float[] GetShapeNormals()
+        {
+            return new float[0];
+        }
+
+        public float[] GetShapeUvCoordinates()
+        {
+            return new float[0];
+        }
+    }
+}
---
+++ Assets/Tests/org/ethasia/adventuregrid/ioadapters/presenters/chunks/ChunkPresentingTests/StandardBlockVisualsBuilderTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+using Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks;
+
+namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks.ChunkPresentingTests
+{
+    public class StandardBlockVisualsBuilderTest
+    {
+        [Test]
+        public void TestThatBuildingVisualsWithoutBlockReturnsZeroBuffers()
+        {
+            StandardBlockVisualsBuilder testCandidate = new StandardBlockVisualsBuilder();
+
+            testCandidate.Build();
+
+            float[] shapePositions = testCandidate.GetShapePositions();
+            int[] shapeTriangleIndices = testCandidate.GetShapeIndices();
+            float[] faceNormals = testCandidate.GetShapeNormals();
+            float[] uvCoordinates = testCandidate.GetShapeUvCoordinates();
+
+            Assert.That(shapePositions.Length, Is.EqualTo(0));
+            Assert.That(shapeTriangleIndices.Length, Is.EqualTo(0));
+            Assert.That(faceNormals.Length, Is.EqualTo(0));
+            Assert.That(uvCoordinates.Length, Is.EqualTo(0));    
+        }   
+    }
+}
