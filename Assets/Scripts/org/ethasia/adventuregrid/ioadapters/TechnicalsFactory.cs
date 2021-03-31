793fd5c418dc0b43fd12412a5b3db52acf623f79
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2021-03-31 20:31:16 +0200

    add: ChunkRenderer and abstract factory to create it.

---
+++ Assets/Scripts/org/ethasia/adventuregrid/ioadapters/TechnicalsFactory.cs
@@ -0,0 +1,21 @@
+﻿using Org.Ethasia.Adventuregrid.Ioadapters.Presenters.interfaces.Technical;
+
+namespace Org.Ethasia.Adventuregrid.Ioadapters
+{
+    public abstract class TechnicalsFactory
+    {
+        private static TechnicalsFactory instance;
+
+        public static void SetInstance(TechnicalsFactory value)
+        {
+            instance = value;
+        }
+
+        public static TechnicalsFactory GetInstance()
+        {
+            return instance;
+        }
+
+        public abstract ChunkRenderer GetChunkRendererInstance();
+    }
+}
---
+++ Assets/Scripts/org/ethasia/adventuregrid/ioadapters/presenters/interfaces/technical/ChunkRenderer.cs
@@ -0,0 +1,9 @@
+﻿using Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks;
+
+namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.interfaces.Technical
+{
+    public interface ChunkRenderer
+    {
+        void RenderChunk(VisualChunkData chunkData);
+    }
+}
