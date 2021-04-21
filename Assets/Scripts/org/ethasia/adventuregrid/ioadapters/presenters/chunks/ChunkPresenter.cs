6ef498332a52be1f00f02fea3162ab3a22c4cdee
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2021-04-21 22:52:10 +0200

    add: empty ChunkPresenter class

---
+++ Assets/Scripts/org/ethasia/adventuregrid/ioadapters/presenters/chunks/ChunkPresenter.cs
@@ -0,0 +1,12 @@
+using Org.Ethasia.Adventuregrid.Core.Environment;
+
+namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks 
+{
+    public class ChunkPresenter
+    {
+        public void PresentChunk(Island island, int chunkCoordinateX, int chunkCoordinateY)
+        {
+
+        }
+    }
+}
--- Assets/Tests/org/ethasia/adventuregrid/ioadapters/presenters/chunks/ChunkPresentingTests/ChunkPresenterTest.cs
+++ Assets/Tests/org/ethasia/adventuregrid/ioadapters/presenters/chunks/ChunkPresentingTests/ChunkPresenterTest.cs
@@ -1,5 +1,9 @@
 using NUnit.Framework;
 
+using Org.Ethasia.Adventuregrid.Core.Environment;
+using Org.Ethasia.Adventuregrid.Core.Math;
+using Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks;
+
 namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks.ChunkPresentingTests
 {
     public class ChunkPresenterTest
@@ -7,7 +11,20 @@ namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks.ChunkPresenting
         [Test]
         public void TestThatRenderingBlocksInZeroZeroChunkIsDoneProperly()
         {
+            Island islandToRender = new Island(16);
 
+            islandToRender.PlaceBlockAt(GrassyEarthBlock.GetInstance(), new BlockPosition(2, 2, 2));
+            islandToRender.PlaceBlockAt(GrassyEarthBlock.GetInstance(), new BlockPosition(3, 2, 2));
+            islandToRender.PlaceBlockAt(GrassyEarthBlock.GetInstance(), new BlockPosition(2, 2, 3));
+            islandToRender.PlaceBlockAt(GrassyEarthBlock.GetInstance(), new BlockPosition(3, 2, 3));
+            islandToRender.PlaceBlockAt(RockBlock.GetInstance(), new BlockPosition(2, 1, 2));
+            islandToRender.PlaceBlockAt(RockBlock.GetInstance(), new BlockPosition(3, 1, 2));
+            islandToRender.PlaceBlockAt(RockBlock.GetInstance(), new BlockPosition(2, 1, 3));
+            islandToRender.PlaceBlockAt(RockBlock.GetInstance(), new BlockPosition(3, 1, 3));
+
+            ChunkPresenter testCandidate = new ChunkPresenter();
+
+            testCandidate.PresentChunk(islandToRender, 0, 0);
         }
     }
 }
