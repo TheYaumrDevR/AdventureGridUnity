cc78a683cf6a2735aa27bc70e3a2ca1ca3fcd4e9
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2021-05-03 19:50:48 +0200

    ChunkPresenter now sets vertex positions completely

--- Assets/Scripts/org/ethasia/adventuregrid/ioadapters/presenters/chunks/ChunkPresenter.cs
+++ Assets/Scripts/org/ethasia/adventuregrid/ioadapters/presenters/chunks/ChunkPresenter.cs
@@ -1,12 +1,24 @@
 using static Org.Ethasia.Adventuregrid.Ioadapters.Presenters.StandardIslandPresenter;
 
+using System.Collections.Generic;
+
 using Org.Ethasia.Adventuregrid.Core.Environment;
+using Org.Ethasia.Adventuregrid.Core.Math;
 using Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Interfaces.Technical;
 
 namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks 
 {
     public class ChunkPresenter
     {
+        private readonly List<float[]> floatBuffersOfOpaqueBlocksInChunk;
+        private readonly List<int[]> intBuffersOfOpaqueBlocksInChunk;   
+
+        public ChunkPresenter() 
+        {
+            floatBuffersOfOpaqueBlocksInChunk = new List<float[]>();
+            intBuffersOfOpaqueBlocksInChunk = new List<int[]>();             
+        }
+
         public void PresentChunk(Island island, int chunkCoordinateX, int chunkCoordinateY)
         {
             VisualChunkData chunkRenderData = ClearRenderDataForNewChunk();
@@ -18,6 +30,9 @@ namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks
 
         private VisualChunkData ClearRenderDataForNewChunk()
         {
+            floatBuffersOfOpaqueBlocksInChunk.Clear();
+            intBuffersOfOpaqueBlocksInChunk.Clear();
+
             return new VisualChunkData();
         }
 
@@ -31,7 +46,23 @@ namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks
                 {
                     for (int islandY = 0; islandY < Island.HEIGHT_IN_BLOCKS; islandY++) 
                     {
-                        
+                        if (islandX < island.GetXzDimension() && islandZ < island.GetXzDimension()) 
+                        {
+                            BlockPosition globalPosition = new BlockPosition(islandX, islandY, islandZ);
+
+                            if (island.GetBlockAt(globalPosition).GetBlockType() != BlockTypes.AIR) 
+                            { 
+                                int inChunkX = islandX - CHUNK_EDGE_LENGTH_IN_BLOCKS * chunkCoordinateX;
+                                int inChunkZ = islandZ - CHUNK_EDGE_LENGTH_IN_BLOCKS * chunkCoordinateY;
+
+                                BlockVisualsBuilder blockVisualsBuilder = BuildBlockVisualsFromBlock(island, globalPosition, inChunkX, inChunkZ, amountOfBlockVerticesAdded);
+                            
+                                if (blockVisualsBuilder.GetShapePositions().Length > 0) 
+                                {   
+                                    FillOpaqueBlockBuffersWithVisualRenderData(blockVisualsBuilder);
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -40,8 +71,51 @@ namespace Org.Ethasia.Adventuregrid.Ioadapters.Presenters.Chunks
         private void RenderChunk(VisualChunkData chunkToRender, int chunkPositionX, int chunkPositionY)
         {
             ChunkRenderer chunkRenderer = TechnicalsFactory.GetInstance().GetChunkRendererInstance();
+            chunkToRender.SetUpWithNumberOfBlocksInChunk(intBuffersOfOpaqueBlocksInChunk.Count);
+            chunkToRender.SetWorldPosition(chunkPositionX, chunkPositionY);
 
+            for (int k = 0; k < floatBuffersOfOpaqueBlocksInChunk.Count; k += 3) 
+            {
+                chunkToRender.AddVerticesToTemporaryBuffer(floatBuffersOfOpaqueBlocksInChunk[k]);
+                chunkToRender.AddNormalsToTemporaryBuffer(floatBuffersOfOpaqueBlocksInChunk[k + 1]);
+                chunkToRender.AddUvCoordinatesToTemporaryBuffer(floatBuffersOfOpaqueBlocksInChunk[k + 2]);
+            }
+
+            for (int k = 0; k < intBuffersOfOpaqueBlocksInChunk.Count; k++) 
+            {
+                chunkToRender.AddIndicesToTemporaryBuffer(new int[0]);
+            }
+
+            chunkToRender.BuildChunkData();
             chunkRenderer.RenderChunk(chunkToRender);
+        }
+
+        private BlockVisualsBuilder BuildBlockVisualsFromBlock(Island island, BlockPosition globalPosition, int inChunkX, int inChunkZ, int currentBlockRenderIndex) 
+        {
+            BlockPosition positionOfBlockInChunk = new BlockPosition(inChunkX, globalPosition.Y, inChunkZ);
+
+            Block currentBlock = island.GetBlockAt(globalPosition);
+            BlockVisualsBuilder blockVisualsBuilder = BlockVisualsBuilder.FromBlockType(currentBlock.GetBlockType());
+            blockVisualsBuilder.SetBlockToCreateDataFrom(currentBlock)
+                .SetPositionOfBlockInChunk(positionOfBlockInChunk)
+                .SetIndexBufferOffsetInChunk(currentBlockRenderIndex)
+                .SetFrontFaceOfBlockIsHidden(island.BlockFaceAtPositionIsHidden(BlockFaceDirections.FRONT, globalPosition))
+                .SetRightFaceOfBlockIsHidden(island.BlockFaceAtPositionIsHidden(BlockFaceDirections.RIGHT, globalPosition))
+                .SetBackFaceOfBlockIsHidden(island.BlockFaceAtPositionIsHidden(BlockFaceDirections.BACK, globalPosition))
+                .SetLeftFaceOfBlockIsHidden(island.BlockFaceAtPositionIsHidden(BlockFaceDirections.LEFT, globalPosition))
+                .SetBottomFaceOfBlockIsHidden(island.BlockFaceAtPositionIsHidden(BlockFaceDirections.BOTTOM, globalPosition))
+                .SetTopFaceOfBlockIsHidden(island.BlockFaceAtPositionIsHidden(BlockFaceDirections.TOP, globalPosition))
+                .Build();  
+
+            return blockVisualsBuilder;
+        }
+
+        private void FillOpaqueBlockBuffersWithVisualRenderData(BlockVisualsBuilder blockRenderDataBuilder) 
+        {
+            floatBuffersOfOpaqueBlocksInChunk.Add(blockRenderDataBuilder.GetShapePositions());
+            floatBuffersOfOpaqueBlocksInChunk.Add(new float[0]);
+            floatBuffersOfOpaqueBlocksInChunk.Add(new float[0]);
+            intBuffersOfOpaqueBlocksInChunk.Add(blockRenderDataBuilder.GetShapeIndices()); 
         }
     }
 }
