c0d5616c550d8200249d55561ed6d17c96c4461a
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2023-01-17 00:53:46 +0100

    Player camera rotation now respects terrain.

--- Assets/Scripts/org/ethasia/adventuregrid/technical/inputcontrols/PlayerCameraInputControl.cs
+++ Assets/Scripts/org/ethasia/adventuregrid/technical/inputcontrols/PlayerCameraInputControl.cs
@@ -5,8 +5,15 @@ namespace Org.Ethasia.Adventuregrid.Technical.Inputcontrols
 {
     public class PlayerCameraInputControl : MonoBehaviour
     {
+        private static readonly Vector3 untranslatedCameraPositionOffset = new Vector3(0, 1.4f, 0);
+        private static readonly Vector3 cameraTranslationVector = new Vector3(0, 0, -4);
+        private static readonly Vector3 cameraForwardPositionScaleVector = new Vector3(0.1f, 0.1f, 0.1f);
+
         [SerializeField]
         private Camera playerCamera;
+
+        [SerializeField]
+        private Transform playerAvatarTransform;
 
         private Vector3 previousCursorViewportPosition;
 
@@ -14,7 +21,6 @@ namespace Org.Ethasia.Adventuregrid.Technical.Inputcontrols
 
         // TODO: make camera attached to player
         // TODO: move logic to layer below
-        // TODO: make camera terrain unpassable
         // TODO: make camera unflippable
 
         void Update()
@@ -30,19 +36,42 @@ namespace Org.Ethasia.Adventuregrid.Technical.Inputcontrols
                 Cursor.visible = false;
 
                 Vector3 cursorDragDirection = previousCursorViewportPosition - playerCamera.ScreenToViewportPoint(Input.mousePosition);
+                Vector3 playerAvatarPosition = playerAvatarTransform.position;
+                Vector3 untranslatedCameraPosition = playerAvatarPosition + untranslatedCameraPositionOffset;
 
-                // playerCamera.transform.localPosition = Vector3.zero;
-                playerCamera.transform.localPosition = new Vector3(150, 65, 94);
+                playerCamera.transform.localPosition = untranslatedCameraPosition;
 
                 playerCamera.transform.Rotate(Vector3.right, cursorDragDirection.y * 180);
                 playerCamera.transform.Rotate(Vector3.up, -cursorDragDirection.x * 180, Space.World);
-                playerCamera.transform.Translate(new Vector3(0, 2, -4));
+                TranslateCameraAfterApplyingRotationWithTerrainCollision(untranslatedCameraPosition);
 
                 Mouse.current.WarpCursorPosition(previousMousePosition);
             }
             else
             {
                 Cursor.visible = true;
+            }
+        }
+
+        private void TranslateCameraAfterApplyingRotationWithTerrainCollision(Vector3 untranslatedCameraPosition) 
+        {
+            RaycastHit hitInfo;
+            bool cameraWillCollideWithTerrain = Physics.Raycast(untranslatedCameraPosition, -playerCamera.transform.forward, out hitInfo, cameraTranslationVector.magnitude);
+
+            if (cameraWillCollideWithTerrain) 
+            {
+                Vector3 veryShortCameraForwardVector = Vector3.Normalize(playerCamera.transform.forward);
+                veryShortCameraForwardVector.Scale(cameraForwardPositionScaleVector);
+
+                Vector3 translationLengthVector = (hitInfo.point - untranslatedCameraPosition) + veryShortCameraForwardVector;
+                Vector3 correctedCameraTranslateVector = Vector3.Normalize(cameraTranslationVector);
+                correctedCameraTranslateVector.Scale(new Vector3(translationLengthVector.magnitude, translationLengthVector.magnitude, translationLengthVector.magnitude));
+
+                playerCamera.transform.Translate(correctedCameraTranslateVector);
+            }
+            else
+            {
+                playerCamera.transform.Translate(cameraTranslationVector);
             }
         }
     }
