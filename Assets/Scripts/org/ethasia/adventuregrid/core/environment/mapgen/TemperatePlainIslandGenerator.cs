4565b8e683602c8030ee3ca0a68f375d00a28f3f
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2023-01-31 23:52:32 +0100

    made IslandGenerator abstract and moved some useful code into it

--- Assets/Scripts/org/ethasia/adventuregrid/core/environment/mapgen/TemperatePlainIslandGenerator.cs
+++ Assets/Scripts/org/ethasia/adventuregrid/core/environment/mapgen/TemperatePlainIslandGenerator.cs
@@ -10,11 +10,7 @@ namespace Org.Ethasia.Adventuregrid.Core.Environment.Mapgen
     public class TemperatePlainIslandGenerator : IslandGenerator
     {
 
-        private Island result;
-        private int[,] heightMap;
-        private HashSet<BlockPosition> coastlineHeightMap;
-
-        public Island GenerateIsland(int edgeLength, HashSet<BlockPosition> coastlineHeightMap) 
+        public override Island GenerateIsland(int edgeLength, HashSet<BlockPosition> coastlineHeightMap) 
         {
             result =  new Island(edgeLength);
             this.coastlineHeightMap = coastlineHeightMap;
@@ -69,86 +65,6 @@ namespace Org.Ethasia.Adventuregrid.Core.Environment.Mapgen
                     }
                 }            
             }
-        }
-
-        private void DisplaceHeightMapByLowestPointOfCoastline()
-        {
-            int maximumHeightDifference = CalculateMaximumHeightDifference();
-            OffsetHeightMapByCoastlineHeightDifference(maximumHeightDifference);           
-        }
-
-        private int CalculateMaximumHeightDifference()
-        {
-            int result = -255;
-            foreach (BlockPosition coastLinePoint in coastlineHeightMap)
-            {
-                if (coastLinePoint.Y > -1)
-                {
-                    int correspondingTerrainPointHeight = heightMap[coastLinePoint.X, coastLinePoint.Z];
-                    int heightDifference = coastLinePoint.Y - correspondingTerrainPointHeight;
-
-                    if (heightDifference > result)
-                    {
-                        result = heightDifference;
-                    }
-                }
-            }    
-
-            return result;        
-        }
-
-        private void OffsetHeightMapByCoastlineHeightDifference(int maximumHeightDifference)
-        {
-            for (int i = 0; i < result.GetXzDimension(); i++) 
-            {
-                for (int j = 0; j < result.GetXzDimension(); j++) 
-                {
-                    int newHeight = heightMap[i, j] + maximumHeightDifference;
-
-                    if (heightMap[i, j] > 0)
-                    {
-                        if (newHeight > 255)
-                        {
-                            newHeight = 255;
-                        } 
-                        else if (newHeight < 0)
-                        {
-                            newHeight = 0;
-                        }
-
-                        heightMap[i, j] = newHeight;
-                    }
-                }
-            } 
-        }
-
-        private void CreateBlocksBasedOnHeightMap() 
-        {
-            for (int i = 0; i < heightMap.GetLength(0); i++) 
-            {
-                for (int j = 0; j < heightMap.GetLength(1); j++) 
-                {
-                    int blockPillarHeight = heightMap[i, j];
-                
-                    for (int k = 0; k < blockPillarHeight; k++) 
-                    {
-                        BlockPosition blockPosition = new BlockPosition(i, k, j);
-
-                        if (k < blockPillarHeight - 2) 
-                        {
-                            result.PlaceBlockAt(RockBlock.GetInstance(), blockPosition);
-                        } 
-                        else if (k < blockPillarHeight - 1) 
-                        {
-                            result.PlaceBlockAt(EarthBlock.GetInstance(), blockPosition);
-                        } 
-                        else 
-                        {
-                            result.PlaceBlockAt(GrassyEarthBlock.GetInstance(), blockPosition);
-                        }
-                    }
-                }
-            }
-        }                   
+        }                  
     }
 }
--- Assets/Scripts/org/ethasia/adventuregrid/core/inputinterfaces/IslandGenerator.cs
+++ Assets/Scripts/org/ethasia/adventuregrid/core/inputinterfaces/IslandGenerator.cs
@@ -5,8 +5,92 @@ using Org.Ethasia.Adventuregrid.Core.Math;
 
 namespace Org.Ethasia.Adventuregrid.Core.InputInterfaces
 {
-    public interface IslandGenerator
+    public abstract class IslandGenerator
     {
-        Island GenerateIsland(int edgeLength, HashSet<BlockPosition> coastlineHeightMap);
+        protected Island result;
+        protected int[,] heightMap;
+        protected HashSet<BlockPosition> coastlineHeightMap;
+
+        public abstract Island GenerateIsland(int edgeLength, HashSet<BlockPosition> coastlineHeightMap);
+
+        protected void DisplaceHeightMapByLowestPointOfCoastline()
+        {
+            int maximumHeightDifference = CalculateMaximumHeightDifference();
+            OffsetHeightMapByCoastlineHeightDifference(maximumHeightDifference);           
+        }
+
+        protected int CalculateMaximumHeightDifference()
+        {
+            int result = -255;
+            foreach (BlockPosition coastLinePoint in coastlineHeightMap)
+            {
+                if (coastLinePoint.Y > -1)
+                {
+                    int correspondingTerrainPointHeight = heightMap[coastLinePoint.X, coastLinePoint.Z];
+                    int heightDifference = coastLinePoint.Y - correspondingTerrainPointHeight;
+
+                    if (heightDifference > result)
+                    {
+                        result = heightDifference;
+                    }
+                }
+            }    
+
+            return result;        
+        }
+
+        protected void OffsetHeightMapByCoastlineHeightDifference(int maximumHeightDifference)
+        {
+            for (int i = 0; i < result.GetXzDimension(); i++) 
+            {
+                for (int j = 0; j < result.GetXzDimension(); j++) 
+                {
+                    int newHeight = heightMap[i, j] + maximumHeightDifference;
+
+                    if (heightMap[i, j] > 0)
+                    {
+                        if (newHeight > 255)
+                        {
+                            newHeight = 255;
+                        } 
+                        else if (newHeight < 0)
+                        {
+                            newHeight = 0;
+                        }
+
+                        heightMap[i, j] = newHeight;
+                    }
+                }
+            } 
+        }
+
+        protected void CreateBlocksBasedOnHeightMap() 
+        {
+            for (int i = 0; i < heightMap.GetLength(0); i++) 
+            {
+                for (int j = 0; j < heightMap.GetLength(1); j++) 
+                {
+                    int blockPillarHeight = heightMap[i, j];
+                
+                    for (int k = 0; k < blockPillarHeight; k++) 
+                    {
+                        BlockPosition blockPosition = new BlockPosition(i, k, j);
+
+                        if (k < blockPillarHeight - 2) 
+                        {
+                            result.PlaceBlockAt(RockBlock.GetInstance(), blockPosition);
+                        } 
+                        else if (k < blockPillarHeight - 1) 
+                        {
+                            result.PlaceBlockAt(EarthBlock.GetInstance(), blockPosition);
+                        } 
+                        else 
+                        {
+                            result.PlaceBlockAt(GrassyEarthBlock.GetInstance(), blockPosition);
+                        }
+                    }
+                }
+            }
+        } 
     }
 }
