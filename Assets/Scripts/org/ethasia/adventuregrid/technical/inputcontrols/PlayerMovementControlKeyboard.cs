dae1e2489e672befb1aeed2b895ec79f6d31948d
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2023-04-24 18:26:07 +0200

    Jumping now triggers at the edges of a block.

--- Assets/Scripts/org/ethasia/adventuregrid/technical/inputcontrols/PlayerMovementControlKeyboard.cs
+++ Assets/Scripts/org/ethasia/adventuregrid/technical/inputcontrols/PlayerMovementControlKeyboard.cs
@@ -39,8 +39,15 @@ namespace Org.Ethasia.Adventuregrid.Technical.Inputcontrols
         public bool PlayerCharacterIsGrounded()
         {
             InitializePlayerWithWorldInteractionControllerIfNull();
-            Position3 playerAvatarFeetPosition = new Position3(playerAvatarTransform.position.x, playerAvatarTransform.position.y, playerAvatarTransform.position.z);
-            return playerWithWorldInteractionController.TestPlayerIsGrounded(playerAvatarFeetPosition);
+            Position3 playerAvatarFeetPositionTopRight = new Position3(playerAvatarTransform.position.x + 0.125f, playerAvatarTransform.position.y, playerAvatarTransform.position.z - 0.125f);
+            Position3 playerAvatarFeetPositionTopLeft = new Position3(playerAvatarTransform.position.x - 0.125f, playerAvatarTransform.position.y, playerAvatarTransform.position.z - 0.125f);
+            Position3 playerAvatarFeetPositionBottomLeft = new Position3(playerAvatarTransform.position.x - 0.125f, playerAvatarTransform.position.y, playerAvatarTransform.position.z + 0.125f);
+            Position3 playerAvatarFeetPositionBottomRight = new Position3(playerAvatarTransform.position.x + 0.125f, playerAvatarTransform.position.y, playerAvatarTransform.position.z + 0.125f);
+            
+            return playerWithWorldInteractionController.TestPlayerIsGrounded(playerAvatarFeetPositionTopRight)
+                || playerWithWorldInteractionController.TestPlayerIsGrounded(playerAvatarFeetPositionTopLeft)
+                || playerWithWorldInteractionController.TestPlayerIsGrounded(playerAvatarFeetPositionBottomLeft)
+                || playerWithWorldInteractionController.TestPlayerIsGrounded(playerAvatarFeetPositionBottomRight);
         }
 
         private void InitializePlayerWithWorldInteractionControllerIfNull()
